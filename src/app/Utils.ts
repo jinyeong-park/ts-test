f0212c22e9b3ff7d9d89279f6b7e0bf8ab138618
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-04-07 15:55:53 -0700

    Add jest hooks - beforeAll, beforeEach

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,5 +1,13 @@
+import { UrlWithParsedQuery, parse } from "url";
+
+
 export class Utils {
+  // parsing url
+  public static parseUrl(url: string): UrlWithParsedQuery {
+    return parse(url, true);
+  }
+
   public static toUpperCase(arg: string) {
-    return '';
+    return arg.toUpperCase();
   }
 }
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,9 +1,41 @@
 import { Utils } from '../app/Utils';
 
-describe('Utils test suite', ()=>{
+describe('Utils test suite', () => {
+
+  // Runs a function before each of the tests in this file runs. If the function returns a promise or is a generator, Jest waits for that promise to resolve before running the test.
+  beforeEach(() => {
+    console.log('before each');
+  });
+
+  // Runs a function before any of the tests in this file run. If the function returns a promise or is a generator, Jest waits for that promise to resolve before running tests.
+  beforeAll(() => {
+    console.log('before all');
+  });
+
+
   test('first test', () => {
-    const abc = Utils.toUpperCase('abc');
-    console.log("check setup works!")
+    const result = Utils.toUpperCase('abc');
+    expect(result).toBe('ABC');
+  })
+
+  test('parse simple URL', () => {
+    const parseUrl = Utils.parseUrl('http://localhost:8080/login');
+    // toBe: compare primitive value
+    expect(parseUrl.href).toBe('http://localhost:8080/login');
+    expect(parseUrl.port).toBe('8080');
+    expect(parseUrl.protocol).toBe('http:');
+    // toEqual : compare object
+    expect(parseUrl.query).toEqual({});
+  });
+
+  test('parse URL with query', () => {
+    const parseUrl = Utils.parseUrl('http://localhost:8080/login?user=user&password=pass');
+    const expectedQuery = {
+      user: 'user',
+      password: 'pass'
+    }
+    expect(parseUrl.query).toEqual(expectedQuery);
+    expect(expectedQuery).toBe(expectedQuery);
   })
 
 })
