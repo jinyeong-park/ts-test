f7598272fd2512ed80932e91e65e99ea6ddf8441
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-04-07 14:56:50 -0700

    Create first simple test with Utils.ts

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,9 @@
+import { Utils } from '../app/Utils';
+
+describe('Utils test suite', ()=>{
+  test('first test', () => {
+    const abc = Utils.toUppercase('abc');
+    console.log("check setup works!")
+  })
+
+})
