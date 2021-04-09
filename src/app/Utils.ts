ce79b746aaeff5490362976058ee2e33a60d8f31
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-04-08 22:04:11 -0700

    test errors

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -4,6 +4,9 @@ import { UrlWithParsedQuery, parse } from "url";
 export class Utils {
   // parsing url
   public static parseUrl(url: string): UrlWithParsedQuery {
+    if (!url) {
+      throw new Error('Empty url');
+    }
     return parse(url, true);
   }
 
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,3 +1,4 @@
+import { textSpanContainsTextSpan } from 'typescript';
 import { Utils } from '../app/Utils';
 
 describe('Utils test suite', () => {
@@ -37,5 +38,33 @@ describe('Utils test suite', () => {
     expect(parseUrl.query).toEqual(expectedQuery);
     expect(expectedQuery).toBe(expectedQuery);
   })
+
+  // Test errors : wrapping function, toThrow, toBeinstanceOf, toHaveProperty
+  // way1
+  // test.only('test invalid URL', () => {
+  test('test invalid URL', () => {
+    // wrap the error with functioon
+    function expectError() {
+      Utils.parseUrl('')
+    }
+    expect(expectError).toThrow('Empty url');
+  })
+  // way2
+  test('test invalid url with arrow function', () => {
+    expect(() => {
+      Utils.parseUrl('')
+    }).toThrow('Empty url');
+  })
+
+  // way3
+  test.only('test invalid URL with try catch', () => {
+    try {
+      Utils.parseUrl('');
+    } catch(error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error).toHaveProperty('message', 'Empty url!');
+    }
+  })
+
 
 })
